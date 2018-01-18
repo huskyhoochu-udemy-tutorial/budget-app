fc7bbab7df92b892db36695af6d37d22b86d8203
huskyhoochu <dfg1499@gmail.com>
2018-01-18 19:43:54 +0900

    CSS selector를 private화

--- app.js
+++ app.js
@@ -7,22 +7,42 @@ var budgetController = (function () {
 // UI 컨트롤러
 var UIController = (function () {
 
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).valueAsNumber
+            };
+        },
+        
+        getDOMStrings: function () {
+            return DOMStrings;
+        }
+    };
+
 })();
 
 // 글로벌 앱 컨트롤러
 var controller = (function (budgetCtrl, UICtrl) {
 
+    // UIController에 정의해 둔 CSS 선택자를 꺼낸다
+    var DOM = UICtrl.getDOMStrings();
+
     // HTML 문서에서 필요한 DOM 객체만 가져와 item으로 가공하는 함수
     var ctrlAddItem = function () {
 
         //    1. input data를 item에  담기
-        var type = document.querySelector('.add__type');
-        var select = type.options[type.selectedIndex].text;
-
-        var description = document.querySelector('.add__description').value;
-
-        var value = document.querySelector('.add__value').valueAsNumber;
-
+        var input = UICtrl.getInput();
+        console.log(input);
 
         //    2. item을 budget controller에 넘기기
 
@@ -32,12 +52,11 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         //    5. 예산을 UI에 띄우기
 
-        console.log(select + " " + description + " " + value);
 
     };
 
     // 이벤트 리스너 1. 체크 버튼을 누를 경우
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     // 이벤트 리스너 2. enter 키를 누를 경우
     document.addEventListener('keypress', function (event) {
